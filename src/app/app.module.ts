65896e2c3d0178f0d3796fca42129affd5085810
AlexMoraga <alex.moraga94@gmail.com>
2018-09-06 11:33:52 -0400

    frontpage animations complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { MaterialModule } from './material';
 import { LoginComponent } from './Pages/login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { RecipeComponent } from './Components/recipe/recipe.component';
 import { DashboardComponent } from './Pages/dashboard/dashboard.component';
@@ -14,6 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FrontpageComponent } from './Pages/frontpage/frontpage.component';
 import { AnimateComponent } from './animate/animate.component';
+
 
 @NgModule({
   declarations: [
