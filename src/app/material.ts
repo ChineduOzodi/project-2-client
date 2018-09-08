7953b84854305ec2e9d4b865dc5db932c65ef626
AlexMoraga <alex.moraga94@gmail.com>
2018-09-07 20:23:59 -0400

    snackbar has been added and log in for is semi-complete

--- src/app/material.ts
+++ src/app/material.ts
@@ -4,9 +4,13 @@ import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatFormFieldModule, MatSelectModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatFormFieldModule, MatSelectModule],
+    imports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatSnackBarModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatSnackBarModule],
 })
 export class MaterialModule { }
