797a54aa3f11e791f05ba6c31ec6ae8041cc0016
Joe Shannon <prjctmedia@USF-Gold-Wifi-226-59-44.laptops.usf.edu>
2018-09-09 13:09:08 -0400

    basic search component complete. adding further search parameter functionality now.

--- src/app/Components/recipe/recipe.component.ts
+++ src/app/Components/recipe/recipe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NutrientService } from '../../api/nutrient.service';
+import { DataService } from '../../Services/data.service';
 
 @Component({
   selector: 'app-recipe',
@@ -6,10 +8,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
+  title = ('Please Search');
+  results = '';
+  myHero = 'Windstorm';
+  food: Object[];
+  nutrient: Object;
+  measure: Object[];
+  itemList;
+  size;
+  selectedMeasure: string;
 
-  constructor() { }
+  acceptableNutrients =
+    [539, 269, 208, 203, 204,
+    205, 297, 295, 291, 301,
+    303, 304, 305, 306, 307,
+    309, 312, 315, 317, 401,
+    404, 405, 406, 415, 435,
+    418, 578, 318, 320, 323,
+    573, 324, 430, 606, 645,
+    646, 605, 601];
 
+
+  constructor(private ns: NutrientService, private ds: DataService) { }
   ngOnInit() {
   }
+// FREE SEARCH FUNCTION //
+search(string, select) {
+this.ds.searchData(string , select).subscribe(search => {
+  this.itemList = search.list.item;
+});
+}
+
+// DETAILS BY ITEM NUMBER
+moreData(data) {
+  console.log(data);
+this.ds.specificData(data).subscribe(selected => {
+  this.food = selected.foods[0].food.desc;
+  this.nutrient = selected.foods[0].food.nutrients;
+  this.measure = selected.foods[0].food.nutrients[0].measures;
+  this.selectedMeasure = this.measure[0].label;
+}
+);
+  }
+
+  checkFilter(x): Boolean {
+
+    if ( this.acceptableNutrients.includes(x)) {
+      console.log('passed: ' + x);
+      return true;
+    } else {
+      console.log('failed: ' + x);
+return false;
+    }
+
+  }
+
+
 
 }
--- src/app/Services/data.service.ts
+++ src/app/Services/data.service.ts
@@ -6,13 +6,61 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+
+
+
 export class DataService {
+
+  api; // the api will be instantiated in each method.
+
+  // apiKey to enter the program, alterable by devs if deprecates.
+  apiKey = 'api_key=iuHaJToUfme3uWpAq2m4qcjd0PJ4JT5csxWX6ey4';
+
+
+                         // core URLs for the API
+
+  // Search by NDBNO (food id num) = ndbno=######& , type = f means full data report returned.
+  reportsURL = `https://api.nal.usda.gov/ndb/V2/reports?type=f&${this.apiKey}&`;
+  ndbno; // part of the URL, it provides the specific info.
+  num; // User clicks on options to see nutritional data.
+
+
+  // Free Search by String
+  searchURL = `https://api.nal.usda.gov/ndb/search/?${this.apiKey}&ds=Standard%20Reference&`;
+   query; // user can free-search a string using this. query is the input information.
+  fg;  // this is a drop-down select of the options.
+  q;
+group;
+
+
 
   constructor(private http: HttpClient) { }
   // urls setted for each specific httprequest tast
 
 
 
+searchData(searchParam, fgParam) {
+
+  searchParam.replace(/' '/g, '%20');  // URL Encoding the Search Request **FIX**
+  this.query = searchParam; // User Query information. Plugs into the q variable.
+  this.group = fgParam; // The Food Group user chose from the drop-down.
+
+  // to keep the url able to easily be edited in the future.
+  this.q = `q=${this.query}&`;
+  this.fg = `fg=${this.group}`;
+
+  // setting the api as whatever was selected.
+  this.api = this.searchURL + this.q + this.fg;
+
+return this.http.get(this.api);
+}
+
+specificData(ndbno) {
+  this.num = ndbno;
+  ndbno = `ndbno=${this.num}`;
+  this.api = this.reportsURL + ndbno;
+  return this.http.get<Object[]>(this.api);
+}
 
 
 
