9afdbd10f896cf34aa725f836a7cb12f0c3b47a3
chin24 <chin2413@gmail.com>
2018-09-11 22:21:36 -0400

    Revert "Revert "Dev""
    This reverts commit e312bc1aacd6f82939b43257566ea802f6673f7d.

---
+++ src/app/Models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    u_id?: number;
+    username: string;
+    password: string;
+    email: string;
+    f_name: string;
+    l_name: string;
+    height?: number;
+    weight?: number;
+    age?: number;
+    sex?: number;
+}
--- src/app/Pages/user-dashboard/user-dashboard.component.ts
+++ src/app/Pages/user-dashboard/user-dashboard.component.ts
@@ -51,3 +51,4 @@ export class UserDashboardComponent implements OnInit {
   }
 
 }
+
