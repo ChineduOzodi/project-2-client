423501261a17f85e870c5f1749a6f39d21b76684
Chinedu Ozodi <chinedu.ozodi@gmail.com>
2018-09-13 14:46:44 -0400

    Revert "fixed more errors"
    This reverts commit 0173aa2b75dfb626675fe29ba313e60d44076d33.

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,33 +1,34 @@
 // ts file for the use for Angular Materials
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCardModule } from '@angular/material/card';
-import {
-    MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule,
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import { MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule,
     MatButtonToggleModule, MatChipsModule, MatDatepickerModule,
     MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule,
     MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule,
     MatRadioModule, MatRippleModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule,
     MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule,
-    MatTooltipModule, MatTreeModule
-} from '@angular/material';
-
-const modules = [
-    MatButtonModule, MatCheckboxModule, MatSidenavModule, MatFormFieldModule,
-    MatSelectModule, MatCardModule, MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule,
-    MatButtonToggleModule, MatChipsModule, MatDatepickerModule,
-    MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule,
-    MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule,
-    MatRadioModule, MatRippleModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule,
-    MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule,
-    MatTooltipModule, MatTreeModule
-];
+    MatTooltipModule, MatTreeModule } from '@angular/material';
 
 @NgModule({
-    imports: [...modules],
-    exports: [...modules],
+    imports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatFormFieldModule,
+        MatSelectModule, MatCardModule, MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule,
+        MatButtonToggleModule, MatChipsModule, MatDatepickerModule,
+        MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule,
+        MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule,
+        MatRadioModule, MatRippleModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule,
+        MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule,
+        MatTooltipModule, MatTreeModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatFormFieldModule, MatSelectModule, MatCardModule,
+        MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule,
+        MatButtonToggleModule, MatChipsModule, MatDatepickerModule,
+        MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule,
+        MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule,
+        MatRadioModule, MatRippleModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule,
+        MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule,
+        MatTooltipModule, MatTreeModule],
 })
 export class MaterialModule { }
