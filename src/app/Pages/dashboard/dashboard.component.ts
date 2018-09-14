39fb15cbe3f4df971c12ea8901a4e2e918d2cd8b
AlexMoraga <alex.moraga94@gmail.com>
2018-09-14 15:49:41 -0400

    userdashboard is semi-complete, going to pull from dev to finish it off

--- src/app/Pages/dashboard/dashboard.component.ts
+++ src/app/Pages/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
+import { DialogEditUserInfoComponent } from './../../DialogBoxes/dialog-edit-user-info/dialog-edit-user-info.component';
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, animate, transition, group } from '@angular/animations';
 import { MatSnackBar } from '@angular/material';
-
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import {FlatTreeControl} from '@angular/cdk/tree';
 
 @Component({
   selector: 'app-dashboard',
@@ -51,15 +54,28 @@ import { MatSnackBar } from '@angular/material';
 
 export class DashboardComponent implements OnInit {
 
+  userName: String = 'Alex Moraga';
+
   state: String = 'state1';
 
   stateOne: String = 'state2';
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
     this.state = 'state1';
     this.stateOne = 'state2';
+  }
+
+  openEditInfo(): void {
+    const dialogRef = this.dialog.open(DialogEditUserInfoComponent, {
+    width: '300px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log(result);
+    });
   }
 
 }
