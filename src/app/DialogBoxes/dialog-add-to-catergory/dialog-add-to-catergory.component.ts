278c05a7fc6a26911ed7b71a246308459666bc08
Chinedu Ozodi <chinedu.ozodi@gmail.com>
2018-09-17 09:34:22 -0400

    more fixes and integration

--- src/app/DialogBoxes/dialog-add-to-catergory/dialog-add-to-catergory.component.ts
+++ src/app/DialogBoxes/dialog-add-to-catergory/dialog-add-to-catergory.component.ts
@@ -1,36 +1,54 @@
+import { UserService } from './../../Services/user.service';
+import { FoodService } from './../../Services/food.service';
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 
 @Component({
- selector: 'app-dialog-add-to-catergory',
- templateUrl: './dialog-add-to-catergory.component.html',
- styleUrls: ['./dialog-add-to-catergory.component.css']
+  selector: 'app-dialog-add-to-catergory',
+  templateUrl: './dialog-add-to-catergory.component.html',
+  styleUrls: ['./dialog-add-to-catergory.component.css']
 })
 export class DialogAddToCatergoryComponent implements OnInit {
   selected = 'option2';
 
- constructor(public snackBar: MatSnackBar) { }
+  constructor(public snackBar: MatSnackBar,
+    private foodService: FoodService,
+    private userService: UserService
+  ) { }
 
- ngOnInit() {
- }
+  ngOnInit() {
+  }
 
- saveCategory() {
-   console.log('value');
- }
+  saveCategory() {
+    console.log('value');
+  }
 
   addCatOne() {
+    this.foodService.saveFood(this.foodService.foodToSave.ndbno, 1, 0, 1, Date.now(), this.userService.user.value.uId).subscribe(() => {
+      console.log('Saved to db');
+      this.foodService.getFoodsFromDb();
+    });
     this.snackBar.open('You have added a breakfast item to your list!', '', {
       duration: 2000,
     });
     console.log('1');
+    console.log('Saving food to db');
   }
   addCatTwo() {
+    this.foodService.saveFood(this.foodService.foodToSave.ndbno, 2, 0, 1, Date.now(), this.userService.user.value.uId).subscribe(() => {
+      console.log('Saved to db');
+      this.foodService.getFoodsFromDb();
+    });
     this.snackBar.open('You have added a lunch item to your list!', '', {
       duration: 2000,
     });
     console.log('2');
   }
   addCatThree() {
+    this.foodService.saveFood(this.foodService.foodToSave.ndbno, 3, 0, 1, Date.now(), this.userService.user.value.uId).subscribe(() => {
+      console.log('Saved to db');
+      this.foodService.getFoodsFromDb();
+    });
     this.snackBar.open('You have added a dinner item to your list!', '', {
       duration: 2000,
     });
